5b283d98a60e01715b6c05460a69f29359e389df
root <biswas1995baban@gmail.com>
2019-10-18 06:00:29 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/yelp_camp",{useNewUrlParser:true, useUnifiedTopology:true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+
+//SETUP SCHEMA
+var campgroundSchema = new mongoose.Schema({
+	name : String,
+	image : String,
+	description : String
+});
+
+// Compiling the schema into model
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//Campground.create({
+	//name:"Dodabeta Top",
+	//image:"https://pixabay.com/get/57e8d1454b56ae14f6da8c7dda793f7f1636dfe2564c704c73267bd1904bc15a_340.jpg"
+//},
+//function(err,Campground){
+	//if(err){
+		//console.log(err);
+	//}
+	//else{
+		//console.log("Newly added campground :");
+		//console.log(Campground);
+	//}
+//});
+
+
+
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+//Get all the campgrounds from database
+//INDEX route -- to show all campgrounds
+app.get("/campgrounds",function(req,res){
+	Campground.find({},function(err,allCampgrounds){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.render("index", {campgrounds:allCampgrounds});
+		}
+	});
+	
+});
+
+//CREATE rote  -- Add new campground to db
+app.post("/campgrounds", function(req,res){
+	var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+	var newCampground = {name:name, image:image, description:description};
+	// Add item to the database
+	Campground.create(newCampground,function(err,newlyCreated){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+//NEW rote -- show form to create a new campground
+app.get("/campgrounds/new", function(req,res){
+	res.render("new");
+});
+
+//SHOW route  --- info about one campground
+app.get("/campgrounds/:id", function(req,res){
+	//Find campground with provided id
+	Campground.findById(req.params.id, function(err, foundCampground){
+		if(err){
+			console.log(err);
+		}else{
+			//render show template with that campground
+			res.render("show",{campground:foundCampground});
+		}
+	});
+	
+});
+
+
+app.listen(process.env.PORT||3000,process.env.IP,function(){
+	console.log("Yelpcamp server has started");
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+#show page
+* Review the REST Full routes we have seen so far
+* Add description to our campground model
+* Show db.collection.drop()
+* Add a show route/template
+
+RESTFull routes -- There are seven of them
+
+Name              url         Verb              Description
+==============================================================================
+INDEX			/dogs         GET             Show a list of all dogs
+NEW             /dogs/new     GET             Displays a form to make new dog
+CREATE          /dogs         POST            Add new dog to db
+SHOW            /dogs/:id     GET             Show infow about one dog
