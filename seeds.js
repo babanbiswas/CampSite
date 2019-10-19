cabff04404fdd769722c99fba9cb8c2d48038d8e
root <biswas1995baban@gmail.com>
2019-10-19 05:13:43 +0000

    add comment.js, associate comment with each campground

--- seeds.js
+++ seeds.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 
 var data = [
 	{
@@ -27,11 +28,26 @@ function seedDB(){
 			console.log("removed campground");
 			//Add a few campgrounds
 			data.forEach(function(seed){
-				Campground.create(seed, function(err, data){
+				Campground.create(seed, function(err, campground){
 					if(err){
 						console.log(err);
 					}else{
 						console.log("Added a campground");
+						//Create a comment
+						Comment.create(
+						{
+							text : "Awsome place",
+							author : "Baban"
+						}, function(err, comment){
+								if(err){
+									console.log(err);
+								}else{
+									campground.comments.push(comment);
+									campground.save();
+									console.log("Created new comment");
+								}
+							}
+						);
 					}
 				});
 			});
